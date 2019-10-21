ac5eee8d2273989a5df4b1f7e3eda6472e62f49d
Andreas Habel <andreas.habel@dpool.com>
2019-10-21 17:16:12 +0200

    [UPDATE]	removed unused gtm code from 3rd party extension

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,3 @@
 <?php
 
 defined('TYPO3_MODE') or die();
-
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['Aoe\\GoogleTagManager\\ViewHelpers\\DataLayerViewHelper'] = array(
-    'className' => 'Dpool\\Base\\Xclass\\DataLayerViewHelper'
-);
