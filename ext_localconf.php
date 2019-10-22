278dc7bfe1141a29aadd32ed0bbc0751d3641343
Andreas Habel <andreas.habel@dpool.com>
2019-10-22 09:48:24 +0200

    [UPDATE]	added schemaOrg plugin to base extension

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,14 @@
 <?php
 
 defined('TYPO3_MODE') or die();
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    'Dpool.' . $_EXTKEY,
+    'Schema',
+    array(
+        'Schema' => 'inject',
+    ),
+    // non-cacheable actions
+    array(
+    )
+);
