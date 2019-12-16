7bc0e29d376e1decdd0cf37c1bfc2b76a8df5efc
Andreas Habel <andreas.habel@dpool.com>
2019-12-16 10:25:04 +0100

    [UPDATE]	added custom record alias mapper for aspects

---
+++ Classes/Routing/Aspect/RecordPersistedAliasMapper.php
@@ -0,0 +1,77 @@
+<?php
+declare(strict_types=1);
+
+
+namespace Dpool\Base\Routing\Aspect;
+
+
+use TYPO3\CMS\Core\Database\ConnectioxnPool;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
+use TYPO3\CMS\Core\Routing\Aspect\PersistedAliasMapper;
+use TYPO3\CMS\Core\Site\SiteLanguageAwareTrait;
+use TYPO3\CMS\Core\Routing\Aspect\PersistenceDelegate;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Database\Query\Restriction\DeletedRestriction;
+
+
+class RecordPersistedAliasMapper extends PersistedAliasMapper
+{
+    use SiteLanguageAwareTrait;
+
+    /**
+     * @var bool
+     */
+    protected $ignoreEnablefields;
+
+    /**
+     * @param array $settings
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __construct(array $settings)
+    {
+        parent::__construct($settings);
+        $ignoreEnablefields = $settings['ignoreEnablefields'] ?? false;
+        $this->ignoreEnablefields = $ignoreEnablefields;
+    }
+
+    /**
+     * @return PersistenceDelegate
+     */
+    protected function getPersistenceDelegate(): PersistenceDelegate
+    {
+
+        if ($this->persistenceDelegate !== null) {
+            return $this->persistenceDelegate;
+        }
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable($this->tableName)
+            ->from($this->tableName);
+
+        if ($this->ignoreEnablefields) {
+            $queryBuilder
+                ->getRestrictions()
+                ->removeAll()
+                ->add(GeneralUtility::makeInstance(DeletedRestriction::class));
+        }
+
+        $resolveModifier = function (QueryBuilder $queryBuilder, array $values) {
+            return $queryBuilder->select(...$this->persistenceFieldNames)->where(
+                ...$this->createFieldConstraints($queryBuilder, $values)
+            );
+        };
+        
+        $generateModifier = function (QueryBuilder $queryBuilder, array $values) {
+            return $queryBuilder->select(...$this->persistenceFieldNames)->where(
+                ...$this->createFieldConstraints($queryBuilder, $values)
+            );
+        };
+
+        return $this->persistenceDelegate = new PersistenceDelegate(
+            $queryBuilder,
+            $resolveModifier,
+            $generateModifier
+        );
+
+    }
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -12,3 +12,6 @@ defined('TYPO3_MODE') or die();
     array(
     )
 );
+
+// Add custom record aspect alias mapping for enabling preview of hidden records
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['routing']['aspects']['RecordPersistedAliasMapper'] = \Dpool\Base\Routing\Aspect\RecordPersistedAliasMapper::class;
