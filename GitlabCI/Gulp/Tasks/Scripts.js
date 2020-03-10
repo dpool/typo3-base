2538cf5871dd6683bdad2c4e0777fce8a7dbc755
Andreas Habel <andreas.habel@dpool.com>
2020-03-10 10:09:01 +0100

    [UPDATE]	exchanged uglify component in gulp process: using uglify-es now instead

--- GitlabCI/Gulp/Tasks/Scripts.js
+++ GitlabCI/Gulp/Tasks/Scripts.js
@@ -7,7 +7,7 @@
 */
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var uglify = require('gulp-uglify-es').default;
 var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
 
