c36c8508248451156e52269a1a9f14277d7ebb51
Andreas Habel <andreas.habel@dpool.com>
2020-03-18 09:41:40 +0100

    [UPDATE]	refactrored schema plugin registration

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,14 +3,11 @@
 defined('TYPO3_MODE') or die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Dpool.' . $_EXTKEY,
+    'Base',
     'Schema',
-    array(
-        'Schema' => 'inject',
-    ),
-    // non-cacheable actions
-    array(
-    )
+    [\Dpool\Base\Controller\SchemaController::class => 'inject'],
+    [\Dpool\Base\Controller\ContactController::class => ''],
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::PLUGIN_TYPE_PLUGIN
 );
 
 // Add custom record aspect alias mapping for enabling preview of hidden records
